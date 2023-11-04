a7122562d089d0a2c12a28ff9e6479194207312d
JumbleBumble <113152254+JumbleBumble@users.noreply.github.com>
2023-11-04 01:39:30 -0400

    Add files via upload

---
+++ examples/AutoTrainerImage-Example.py
@@ -0,0 +1,10 @@
+from library import AutoAimTrainer #Assuming you have the library in a folder named library and named it AutoAimTrainer
+
+  
+y_min = 331
+x_min = 567
+y_max = 760
+x_max = 1478
+image = 'target.PNG'
+AutoAimTrainer.AutoTrainerImage(image,0,[x_min,y_min],[x_max,y_max])
+
